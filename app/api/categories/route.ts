e3bbd7cb35862dd3802c01acf635465cb8eee6c7
apo5698 <winteryao17@gmail.com>
2024-02-26 23:41:56 -0500

    feat(db): sort categories by pinyin

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,11 +1,17 @@
 import { prisma } from '@/prisma/client';
 
 export async function GET() {
-  const result = await prisma.category.findMany({
-    select: {
-      name: true,
-    },
-  });
+  const result = await prisma.category
+    .findMany({
+      select: {
+        name: true,
+        namePinyin: true,
+      },
+    })
+    .then((result) => {
+      // Prisma does not support order by computed values
+      return result.sort((a, b) => a.namePinyin.localeCompare(b.namePinyin));
+    });
 
   return Response.json(result);
 }
--- prisma/client.ts
+++ prisma/client.ts
@@ -1,3 +1,13 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Restaurant } from '@prisma/client';
+import { pinyin } from 'pinyin-pro';
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient().$extends({
+  result: {
+    category: {
+      namePinyin: {
+        needs: { name: true },
+        compute: (category) => pinyin(category.name, { toneType: 'none' }),
+      },
+    },
+  },
+});
