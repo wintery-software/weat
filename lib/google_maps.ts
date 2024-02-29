24c2adf667abda2c3d47c0541a2081fd295205e4
apo5698 <winteryao17@gmail.com>
2024-02-28 19:16:52 -0500

    build: add libraries

---
+++ lib/google_maps.ts
@@ -0,0 +1,55 @@
+import { meterToMile } from '@/lib/constants';
+import { Client, UnitSystem } from '@googlemaps/google-maps-services-js';
+
+export type DistanceReturnType = {
+  // Distance in miles
+  distance: number;
+  // Duration in minutes
+  duration: number;
+};
+
+export const isGoogleMapsApiEnabled = () =>
+  process.env.NEXT_PUBLIC_ENABLE_GOOGLE_MAPS_API === 'true';
+
+const getClient = () => {
+  if (!isGoogleMapsApiEnabled()) {
+    throw new Error('Google Maps API is disabled');
+  }
+
+  return new Client({});
+};
+
+export const calculateDistance = async (
+  originCoordinates: [number, number],
+  destinationPlaceId: string,
+): Promise<DistanceReturnType> => {
+  const client = getClient();
+
+  if (!Array.isArray(originCoordinates) || originCoordinates.length !== 2) {
+    throw new Error(`Invalid origin coordinates: ${originCoordinates}`);
+  }
+
+  if (!destinationPlaceId) {
+    throw new Error('Missing destination place ID');
+  }
+
+  if (!process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY) {
+    throw new Error('Missing Google Maps API key');
+  }
+
+  const response = await client.distancematrix({
+    params: {
+      origins: [originCoordinates],
+      destinations: [`place_id:${destinationPlaceId}`],
+      units: UnitSystem.imperial,
+      key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    },
+  });
+
+  const result = response.data.rows[0].elements[0];
+
+  return {
+    distance: meterToMile(result.distance.value),
+    duration: result.duration.value / 60,
+  };
+};
