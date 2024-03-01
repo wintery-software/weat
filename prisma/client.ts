13a3d9339cf6a2f2293ac1da481e8df55f3c87bb
apo5698 <winteryao17@gmail.com>
2024-03-01 17:53:45 -0500

    chore: remove unused import

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Restaurant } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { pinyin } from 'pinyin-pro';
 
 export const prisma = new PrismaClient().$extends({
