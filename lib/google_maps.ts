37f3ba9205cfed2f914cf2d59996194c5d952424
apo5698 <winteryao17@gmail.com>
2024-03-02 18:51:48 -0500

    feat(lib): add getPlaceDetails()

--- lib/google_maps.ts
+++ lib/google_maps.ts
@@ -1,5 +1,24 @@
 import { meterToMile } from '@/lib/constants';
-import { Client, UnitSystem } from '@googlemaps/google-maps-services-js';
+import {
+  Client,
+  PlacePhoto,
+  UnitSystem,
+} from '@googlemaps/google-maps-services-js';
+import { ResponseData } from '@googlemaps/google-maps-services-js/src/common';
+
+export class GoogleMapsApiError extends Error {
+  constructor(message: string, ...args: any[]) {
+    super(message, ...args);
+    this.name = 'GoogleMapsApiError';
+  }
+}
+
+export class GoogleMapsApiInitializationError extends GoogleMapsApiError {
+  constructor(message: string, ...args: any[]) {
+    super(message, ...args);
+    this.name = 'GoogleMapsApiInitializationError';
+  }
+}
 
 export type DistanceReturnType = {
   // Distance in miles
@@ -15,7 +34,9 @@ const getApiKey = (): string => {
   const apiKey = process.env.GOOGLE_MAPS_API_KEY;
 
   if (!apiKey) {
-    throw new Error('Google Maps API key is not set');
+    throw new GoogleMapsApiInitializationError(
+      'Google Maps API key is not set',
+    );
   }
 
   return apiKey;
@@ -23,7 +44,7 @@ const getApiKey = (): string => {
 
 const getClient = (): Client => {
   if (!isGoogleMapsApiEnabled()) {
-    throw new Error('Google Maps API is disabled');
+    throw new GoogleMapsApiInitializationError('Google Maps API is disabled');
   }
 
   return new Client({});
@@ -59,4 +80,58 @@ export const calculateDistance = async (
     distance: meterToMile(result.distance.value),
     duration: result.duration.value / 60,
   };
+};
+
+export const getPlaceDetails = async (placeId: string) => {
+  const apiKey = getApiKey();
+  const client = getClient();
+
+  try {
+    const response = await client.placeDetails({
+      params: {
+        place_id: placeId,
+        fields: [
+          'name',
+          'formatted_address',
+          'geometry',
+          'place_id',
+          'types',
+          'price_level',
+          'rating',
+          'photos',
+        ],
+        key: apiKey,
+      },
+    });
+
+    const place = response.data.result;
+
+    // Short URL with zoom level 19 (200 ft)
+    const googleMapsUrl = `https://www.google.com/maps/place/?q=place_id:${placeId}`;
+
+    const images = place.photos
+      ? place.photos
+          .slice(0, 5)
+          .map(
+            (photo: PlacePhoto) =>
+              `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photo.photo_reference}&key=${apiKey}`,
+          )
+      : [];
+
+    // Format the data
+    return {
+      name: place.name,
+      address: place.formatted_address,
+      googleMapsUrl,
+      googleMapsPlaceId: placeId,
+      // categories: place.types.map((type) => type.replace('_', ' ')),
+      price: place.price_level,
+      rating: place.rating,
+      images,
+    };
+  } catch (e: any) {
+    const error: ResponseData = e.response.data;
+    console.log(error.error_message);
+    throw new GoogleMapsApiError(error.error_message);
+  }
 };
