999a0b8cb976f0a2f04195a5c4d656b8ccc0f1a0
apo5698 <winteryao17@gmail.com>
2024-03-02 18:48:19 -0500

    chore(lib): use HTTP status constants

--- lib/response.ts
+++ lib/response.ts
@@ -1,12 +1,21 @@
+import { constants } from 'node:http2';
+
 export const MessageResponse = (message: string) => Response.json({ message });
 
 export const ErrorResponse = (error: string, status: number) =>
   Response.json({ error }, { status });
 
-export const BadRequest = (error: string) => ErrorResponse(error, 400);
+export const BadRequest = (error: string) =>
+  ErrorResponse(error, constants.HTTP_STATUS_BAD_REQUEST);
 
-export const Unauthorized = (error: string) => ErrorResponse(error, 401);
+export const Unauthorized = (error: string) =>
+  ErrorResponse(error, constants.HTTP_STATUS_UNAUTHORIZED);
 
-export const Forbidden = (error: string) => ErrorResponse(error, 403);
+export const Forbidden = (error: string) =>
+  ErrorResponse(error, constants.HTTP_STATUS_FORBIDDEN);
 
-export const NotFound = (error: string) => ErrorResponse(error, 404);
+export const NotFound = (error: string) =>
+  ErrorResponse(error, constants.HTTP_STATUS_NOT_FOUND);
+
+export const InternalServerError = (error: string) =>
+  ErrorResponse(error, constants.HTTP_STATUS_INTERNAL_SERVER_ERROR);
