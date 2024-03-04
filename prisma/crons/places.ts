571a60c0c2f0043a2b8f46bbbc3b3c4a4fb45c2c
apo5698 <winteryao17@gmail.com>
2024-03-03 23:45:59 -0500

    build: cron for updating places

---
+++ prisma/crons/places.ts
@@ -0,0 +1,22 @@
+import { getPlaceDetails } from '@/lib/google_maps';
+import { prisma } from '@/prisma/client';
+import { Prisma } from '@prisma/client';
+
+const updateRestaurant = async () => {
+  const restaurants = await prisma.restaurant.findMany({});
+
+  const data: Awaited<Prisma.RestaurantCreateInput>[] = await Promise.all(
+    restaurants.map(async (r) => {
+      return getPlaceDetails(r.placeId);
+    }),
+  );
+
+  for (const r of data) {
+    await prisma.restaurant.update({
+      where: {
+        placeId: r.placeId,
+      },
+      data: r,
+    });
+  }
+};
