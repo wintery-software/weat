82d1dc026da270d346460956e27b8619be268483
apo5698 <winteryao17@gmail.com>
2024-03-13 14:53:48 -0400

    feat(ui): add Toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ReactNode, Suspense } from 'react';
+import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Suspense>{children}</Suspense>
         <SpeedInsights />
+        <Toaster />
       </body>
     </html>
   );
