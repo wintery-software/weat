88438fbc47e7e5190196812510013d1202d82594
apo5698 <winteryao17@gmail.com>
2024-03-13 01:03:44 -0400

    feat(ui): custom breadcrumb

---
+++ components/auto_breadcrumb.tsx
@@ -0,0 +1,49 @@
+import { Fragment } from 'react';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from './ui/breadcrumb';
+
+interface AutoBreadcrumbProps {
+  className?: string;
+  parents: { name: string; url: string }[];
+  current: string;
+  prependHome?: boolean;
+}
+
+export default function AutoBreadcrumb({
+  className,
+  parents,
+  current,
+  prependHome = true,
+}: AutoBreadcrumbProps) {
+  if (prependHome) {
+    parents.unshift({ name: '首页', url: '/' });
+  }
+
+  const fontSize = `text-xs md:text-sm`;
+
+  return (
+    <Breadcrumb className={className}>
+      <BreadcrumbList>
+        {parents.map((item, index) => (
+          <Fragment key={index}>
+            <BreadcrumbItem>
+              <BreadcrumbLink className={fontSize} href={item.url}>
+                {item.name}
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+          </Fragment>
+        ))}
+        <BreadcrumbItem>
+          <BreadcrumbPage className={fontSize}>{current}</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
