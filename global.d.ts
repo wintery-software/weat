21247398a5d49fcec76ae0e24f1ef035107cd5f1
apo5698 <winteryao17@gmail.com>
2024-03-14 03:42:49 -0400

    chore: delete global type

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,1 @@
-import { DistanceReturnType } from '@/lib/google-maps';
-import { Restaurant as PrismaRestaurant } from '@prisma/client';
-
-export declare global {
-  type Restaurant = PrismaRestaurant & {
-    categories: string[];
-    distance: Omit<DistanceReturnType, 'origin' | 'destination'>;
-  };
-}
+export declare global {}
