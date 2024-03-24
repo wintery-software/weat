9377dc9d176f17f6e2ea7e8d4ccfaaaed45aeca3
apo5698 <winteryao17@gmail.com>
2024-03-24 18:47:03 -0400

    style: next.config.js

--- next.config.js
+++ next.config.js
@@ -2,43 +2,44 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  headers: async () =>
-    [
-      {
-        // matching all API routes
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT'
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-          }
-        ]
-      }
-    ],
+  headers: async () => [
+    {
+      // matching all API routes
+      source: '/api/:path*',
+      headers: [
+        { key: 'Access-Control-Allow-Credentials', value: 'true' },
+        { key: 'Access-Control-Allow-Origin', value: '*' },
+        {
+          key: 'Access-Control-Allow-Methods',
+          value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+        },
+        {
+          key: 'Access-Control-Allow-Headers',
+          value:
+            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+        },
+      ],
+    },
+  ],
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'wintery-software-weat.s3.amazonaws.com',
-        pathname: '/**/*'
-      }, {
+        pathname: '/**/*',
+      },
+      {
         protocol: 'https',
         hostname: 'via.placeholder.com',
-        pathname: '/*'
-      }
-    ]
+        pathname: '/*',
+      },
+    ],
   },
   logging: {
     fetches: {
-      fullUrl: true
-    }
-  }
+      fullUrl: true,
+    },
+  },
 };
 
 module.exports = nextConfig;
