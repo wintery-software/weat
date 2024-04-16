8ee0cece5e93385c2ef39b95622eee00c68b5315
apo5698 <winteryao17@gmail.com>
2024-04-15 22:22:15 -0400

    feat(i18n): don't use a locale prefix for the default locale
    See: https://next-intl-docs.vercel.app/docs/routing/middleware#matcher-no-prefix

--- middleware.ts
+++ middleware.ts
@@ -8,6 +8,12 @@ export default createMiddleware({
 });
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(en|zh-CN)/:path*'],
+  // Matcher entries are linked with a logical "or", therefore
+  // if one of them matches, the middleware will be invoked.
+  matcher: [
+    // Match all pathnames except for
+    // - … if they start with `/api`, `/_next` or `/_vercel`
+    // - … the ones containing a dot (e.g. `favicon.ico`)
+    '/((?!api|_next|_vercel|.*\\..*).*)',
+  ],
 };
