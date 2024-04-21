ccad207d26526759c0fddaad10c990ca533c5f05
apo5698 <winteryao17@gmail.com>
2024-04-21 17:01:29 -0400

    chore: only enable sentry in production

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -4,15 +4,17 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-Sentry.init({
-  dsn: 'https://3e3d51453a2215e1b290b9e1dbe4b09e@o4507125694267392.ingest.us.sentry.io/4507125695709184',
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://3e3d51453a2215e1b290b9e1dbe4b09e@o4507125694267392.ingest.us.sentry.io/4507125695709184',
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
 
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
-});
+    // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+    // spotlight: process.env.NODE_ENV === 'development',
+  });
+}
