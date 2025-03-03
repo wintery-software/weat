6a3423016e2acff90f913df2b9a85c50685ac41a
apo5698 <winteryao17@gmail.com>
2025-03-02 18:29:13 -0800

    refactor: remove console log from Google Maps API onLoad

--- app/providers.tsx
+++ app/providers.tsx
@@ -8,7 +8,6 @@ export function Providers({ children }: { children: ReactNode }) {
   return (
     <APIProvider
       apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}
-      onLoad={() => console.log("Google Maps API has loaded.")}
       libraries={["core", "marker"]}
     >
       <ThemeProvider
