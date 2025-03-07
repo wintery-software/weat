03f638d66595e077f4ae51f53c05a3a03b8cf0e1
apo5698 <winteryao17@gmail.com>
2025-03-07 14:25:52 -0800

    feat: add bottom border to navigation

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 
 export const Navigation = ({ children }: { children: ReactNode }) => {
   return (
-    <header className="sticky top-0 z-50 bg-background">
+    <header className="sticky top-0 z-50 border-b bg-background">
       <div className="flex h-12 items-center">{children}</div>
     </header>
   );
