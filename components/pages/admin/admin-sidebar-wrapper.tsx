cef598187ff3e22ff8e30261724cc879c8e0c834
apo5698 <winteryao17@gmail.com>
2025-03-08 23:23:59 -0800

    refactor: move AdminLayout to server component with client wrapper

---
+++ components/pages/admin/admin-sidebar-wrapper.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { BreadcrumbItemProps, SidebarGroupProps, SidebarLayout } from "@/components/layouts/sidebar-layout";
+import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
+
+export const AdminLayoutWrapper = ({ children }: { children: ReactNode }) => {
+  const pathname = usePathname();
+
+  const content: SidebarGroupProps[] = [
+    {
+      title: "Overview",
+      items: [
+        {
+          title: "Dashboard",
+          url: "/admin",
+          isActive: pathname === "/admin",
+        },
+      ],
+    },
+    {
+      title: "Data",
+      items: [
+        {
+          title: "Places",
+          url: "/admin/data/places",
+          isActive: pathname === "/admin/data/places",
+        },
+      ],
+    },
+  ];
+
+  const breadcrumbs: BreadcrumbItemProps[] = [
+    { title: "Admin", url: "/admin" },
+    { title: "Dashboard", url: "#" },
+  ];
+
+  return (
+    <SidebarLayout content={content} breadcrumbs={breadcrumbs}>
+      {children}
+    </SidebarLayout>
+  );
+};
