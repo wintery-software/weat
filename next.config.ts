884491069acfba527a49eb63f1b4b1c9a53f5525
apo5698 <winteryao17@gmail.com>
2025-03-12 17:14:26 -0700

    feat: rewrite / to /map

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  rewrites: async () => [
+    {
+      source: "/",
+      destination: "/map",
+    },
+  ],
   transpilePackages: ["lucide-react"],
 };
 
