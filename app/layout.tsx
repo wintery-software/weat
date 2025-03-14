7d54fa718deb2ee675d7b16773a83ff908f6e745
apo5698 <winteryao17@gmail.com>
2025-03-13 17:34:13 -0700

    feat: remove notification dropdown

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,11 @@
 import "./globals.css";
 import { Providers } from "@/app/providers";
-import { Navigation } from "@/components/navigation";
-import Notifications from "@/components/notifications";
-import { Button } from "@/components/ui/button";
 import { Toaster } from "@/components/ui/sonner";
 import { cn } from "@/lib/utils";
-import { SiGithub } from "@icons-pack/react-simple-icons";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { LucideBanana, LucideMapPinPlus } from "lucide-react";
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Link from "next/link";
 import { type ReactNode } from "react";
 
 const geistSans = Geist({
@@ -55,32 +49,6 @@ const RootLayout = ({
     <html lang="en" suppressHydrationWarning>
       <body className={cn(geistSans.variable, geistMono.variable, "antialiased", "flex", "flex-col", "w-dvh", "h-dvh")}>
         <Providers>
-          <Navigation>
-            <Button variant="ghost" asChild className="font-bold hover:bg-background">
-              <Link href="/">
-                <LucideBanana />
-                Weat
-              </Link>
-            </Button>
-            {/* Right side */}
-            <div className="ml-auto flex items-center gap-2 px-2">
-              <Button size={"sm"} variant={"outline"} asChild>
-                <Link href={process.env.GOOGLE_SUGGEST_FORM_URL || "#"} target={"_blank"}>
-                  <LucideMapPinPlus size={12} />
-                  Suggest a new place
-                </Link>
-              </Button>
-              <Notifications />
-              <div className="flex gap-2">
-                <Button variant={"ghost"} size={"icon"} asChild>
-                  <Link href="https://github.com/wintery-software/weat" target="_blank">
-                    <SiGithub />
-                  </Link>
-                </Button>
-                {/*<ThemeToggle />*/}
-              </div>
-            </div>
-          </Navigation>
           <div className="grow">{children}</div>
           <Toaster richColors />
           <Analytics />
