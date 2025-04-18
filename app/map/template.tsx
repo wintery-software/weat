b7be87f51b2f5eb83ddc1f8b5ad06368c578ce35
apo5698 <winteryao17@gmail.com>
2025-04-17 17:01:26 -0700

    feat(map): increase sidebar width on desktop

--- app/map/template.tsx
+++ app/map/template.tsx
@@ -2,11 +2,19 @@
 
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { APIProvider } from "@vis.gl/react-google-maps";
-import { ReactNode } from "react";
+import { CSSProperties, ReactNode } from "react";
 
 const Template = ({ children }: { children: ReactNode }) => (
   <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!} libraries={["core", "marker"]}>
-    <SidebarProvider>{children}</SidebarProvider>
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "24rem",
+        } as CSSProperties
+      }
+    >
+      {children}
+    </SidebarProvider>
   </APIProvider>
 );
 
