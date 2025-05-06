df4375941058de81ceaca04569eece3d8992328f
apo5698 <winteryao17@gmail.com>
2025-05-06 00:38:40 -0700

    feat: set API request timeout to 5000ms

--- lib/api.ts
+++ lib/api.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+export const API_REQUEST_TIMEOUT = 5000;
+
 export const WeatAPI = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
+  timeout: API_REQUEST_TIMEOUT,
 });
