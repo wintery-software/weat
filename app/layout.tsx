66c74d910a09b5996b92b914e75dc7d97a8de7a4
apo5698 <winteryao17@gmail.com>
2025-07-05 11:33:21 -0700

    feat(analytics): add Vercel utilities

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import { Providers } from "@/components/providers";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import type { ReactNode } from "react";
@@ -26,6 +28,8 @@ const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
         className={`${geistSans.variable} ${geistMono.variable}antialiased`}
       >
         <Providers>{children}</Providers>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
