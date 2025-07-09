30f47df07b3d0472534fe8a5dd27b263d10d0566
apo5698 <winteryao17@gmail.com>
2025-07-09 16:38:07 -0700

    refactor(ui): reduce card components padding to 16px

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -6,7 +6,7 @@ const Card = ({ className, ...props }: React.ComponentProps<"div">) => {
     <div
       data-slot="card"
       className={cn(
-        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6",
+        "bg-card text-card-foreground flex flex-col gap-4 rounded-xl border py-4",
         className,
       )}
       {...props}
@@ -19,7 +19,7 @@ const CardHeader = ({ className, ...props }: React.ComponentProps<"div">) => {
     <div
       data-slot="card-header"
       className={cn(
-        "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6",
+        "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-4 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-4",
         className,
       )}
       {...props}
@@ -67,7 +67,7 @@ const CardContent = ({ className, ...props }: React.ComponentProps<"div">) => {
   return (
     <div
       data-slot="card-content"
-      className={cn("px-6", className)}
+      className={cn("px-4", className)}
       {...props}
     />
   );
@@ -77,7 +77,7 @@ const CardFooter = ({ className, ...props }: React.ComponentProps<"div">) => {
   return (
     <div
       data-slot="card-footer"
-      className={cn("flex items-center px-6 [.border-t]:pt-6", className)}
+      className={cn("flex items-center px-4 [.border-t]:pt-4", className)}
       {...props}
     />
   );
