bcc991fb2efbfb0d2ec7445019ac466826aee8bb
apo5698 <winteryao17@gmail.com>
2025-07-12 14:04:07 -0700

    fix(api): force-dynamic admin/tags

--- app/admin/database/tags/layout.tsx
+++ app/admin/database/tags/layout.tsx
@@ -1,6 +1,8 @@
 import { SuspenseWrapper } from "@/components/layouts/suspense-wrapper";
 import { PropsWithChildren } from "react";
 
+export const dynamic = "force-dynamic";
+
 const Layout = ({ children }: PropsWithChildren) => (
   <SuspenseWrapper>{children}</SuspenseWrapper>
 );
