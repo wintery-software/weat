e2c1c5374670d2da4bb73a2ccaba3eea76d8fb88
apo5698 <winteryao17@gmail.com>
2025-07-27 17:25:45 -0700

    chore: add supabase url ot remote image patterns

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["air.local"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
